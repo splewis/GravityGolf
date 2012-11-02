b6417250262a238e1ab62727e05d466c14901862
Sean Lewis <splewis@utexas.edu>
2012-11-02 13:23:32 -0500

    Formatting

--- src/game/GravityGolf.java
+++ src/game/GravityGolf.java
@@ -7,20 +7,19 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 /**
- * 
  * @author Sean Lewis
- *
  */
 
-public class GravityGolf {		
-	public static void main(String[] args) throws  IOException {
+public class GravityGolf {
+	public static void main(String[] args) throws IOException {
 		final GameFrame gf = new GameFrame();
-		gf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 			
+		gf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gf.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent ev) {
-            	gf.safeQuit(); // ensures gamelog.txt and settings.txt will be written to on close
-            	gf.dispose();
-            }
-        });  
-	}	
+			public void windowClosing(WindowEvent ev) {
+				gf.safeQuit(); // ensures gamelog.txt and settings.txt will be
+								// written to on close
+				gf.dispose();
+			}
+		});
+	}
 }
