7839425e11fa171e47b88033989a0cb23336624d
Sean Lewis <splewis@utexas.edu>
2013-01-02 19:14:56 -0600

    Generalized SettingsReader tests to DataHandlerTests

--- src/tests/DataHandlerTests.java
+++ src/tests/DataHandlerTests.java
@@ -2,10 +2,12 @@ package tests;
 
 import static org.junit.Assert.*;
 import game.DataHandler;
+
+import java.awt.Color;
 import java.io.*;
 import org.junit.*;
 
-public class SettingsReader {
+public class DataHandlerTests {
 	
 	static final String dir = "testFiles/";
 	
@@ -15,7 +17,7 @@ public class SettingsReader {
 	}	
 	
 	@Test
-	public void simpleRead() throws FileNotFoundException {
+	public void simpleSettingsRead() throws FileNotFoundException {
 		String fileName = dir + "simpleRead.txt";
 		PrintWriter pw = new PrintWriter(new File(fileName));
 		pw.println("effects = yes");
@@ -30,7 +32,7 @@ public class SettingsReader {
 	}
 
 	@Test
-	public void simpleRead_noTrail() throws FileNotFoundException {
+	public void simpleSettingsRead_noTrail() throws FileNotFoundException {
 		String fileName = dir + "simpleRead_noTrail.txt";
 		PrintWriter pw = new PrintWriter(new File(fileName));
 		pw.println("effects = yes");
@@ -45,7 +47,7 @@ public class SettingsReader {
 	}
 
 	@Test
-	public void badRead() throws FileNotFoundException {
+	public void badSettingsRead() throws FileNotFoundException {
 		String fileName = dir + "badRead.txt";
 		PrintWriter pw = new PrintWriter(new File(fileName));
 		pw.println("effects = yes");
@@ -57,4 +59,25 @@ public class SettingsReader {
 				DataHandler.getSettings(fileName));
 	}
 
+	@Test
+	public void readColorTests() {
+		assertEquals(Color.red, DataHandler.readColor("red"));
+		assertEquals(Color.green, DataHandler.readColor("green"));
+		assertEquals(Color.blue, DataHandler.readColor("blue"));
+		assertEquals(Color.white, DataHandler.readColor("white"));
+		assertEquals(Color.black, DataHandler.readColor("black"));
+		assertEquals(Color.yellow, DataHandler.readColor("yellow"));
+		assertEquals(Color.cyan, DataHandler.readColor("cyan"));
+		
+		assertEquals(new Color(1,2,3), DataHandler.readColor("Color(1, 2, 3)"));
+		assertEquals(new Color(3,2,1), DataHandler.readColor("Color(3, 2, 1)"));
+		assertEquals(new Color(1,2,3), DataHandler.readColor("Color(1, 2, 3))"));
+		assertEquals(new Color(3,2,1), DataHandler.readColor("Color(3, 2, 1))"));
+		
+		assertEquals(null, DataHandler.readColor("Color(3.1, 2, 1)"));
+		assertEquals(null, DataHandler.readColor("Color(4, 3, 2, 1)"));
+		assertEquals(null, DataHandler.readColor("Color(1, 2)"));
+	}
+	
+	
 }
