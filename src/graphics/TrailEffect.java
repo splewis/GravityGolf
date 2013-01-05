e2677a5783c8a0d7248b15cd392718b4516ce21f
Sean Lewis <splewis@utexas.edu>
2013-01-05 01:29:37 -0600

    Refactored to simplify graphics.TrailEffect code

--- src/graphics/TrailEffect.java
+++ src/graphics/TrailEffect.java
@@ -3,6 +3,7 @@ package graphics;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+
 import structures.Level;
 import structures.Point2d;
 
@@ -12,7 +13,7 @@ import structures.Point2d;
  */
 public final class TrailEffect extends GraphicEffect {
 
-	private static ArrayList<Point2d> trailPoints = new ArrayList<Point2d>();
+	private static ArrayList<Point2d> trailPoints = new ArrayList<Point2d>(100);
 
 	/**
 	 * Adds a point the to current trail.
@@ -36,30 +37,27 @@ public final class TrailEffect extends GraphicEffect {
 	 * @param g the Graphics component to draw with
 	 */
 	public static void draw(Level level, Graphics g) {
-		try {
-			double screenXShift = level.getScreenXShift();
-			double screenYShift = level.getScreenYShift();
-			Color c = Color.green;
-			g.setColor(c);
-			for (int i = 0; i < trailPoints.size() - 2; i++) {
-				if (trailPoints.get(i) != null
-						&& trailPoints.get(i + 1) != null) {
-					g.drawLine(
-							(int) (Math.round(trailPoints.get(i).x
-									+ screenXShift)),
-							(int) (Math.round(trailPoints.get(i).y
-									+ screenYShift)),
-							(int) (Math.round(trailPoints.get(i + 1).x
-									+ screenXShift)),
-							(int) (Math.round(trailPoints.get(i + 1).y
-									+ screenYShift)));
-
-				}
-			}
-		} catch (Exception e) {
-
+		double screenXShift = level.getScreenXShift();
+		double screenYShift = level.getScreenYShift();
+		
+		// number of points to draw
+		int n = trailPoints.size() - 2;
+		g.setColor(Color.green);		
+		
+		for (int i = 0; i < n; i++) {
+			Point2d from = trailPoints.get(i);
+			Point2d to   = trailPoints.get(i + 1);
+			int fromX = round(from.x + screenXShift);
+			int fromY = round(from.y + screenYShift);
+			int toX   = round(to.x   + screenXShift);
+			int toY   = round(to.y   + screenYShift);
+			g.drawLine(fromX, fromY, toX, toY);
 		}
-
+	}
+	
+	// convenience method since Math.round returns a long
+	private static int round(double x) {
+		return (int) Math.round(x);
 	}
 
 }
